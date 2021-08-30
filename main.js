aec73e4753dc60d48cfa4a1ac12a0b4300fcb740
anupupgirl <86975247+anupupgirl@users.noreply.github.com>
2021-08-30 14:12:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["dad.jpg","mom.jpg", "me.png" , "sis.jpg", "grandpa.png", "grandma.jpg"];
+var names = ["Dad", "Mom", "Me", "Sister", "grandpa", "grandma"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
